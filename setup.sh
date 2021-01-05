0c14590639c7ca3f6e1f2bab24ca92a8c59bfc6b
r-oli <r-oliveira@ispace-inc.com>
2021-01-05 14:01:55 -0300

    3

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "yum -y update"
+yum -y update
 
-echo "yum install -y yum-utils"
+yum install -y yum-utils
 
-echo "yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo"
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-echo "yum install -y yum install docker-ce docker-ce-cli containerd.io"
+yum install -y yum install docker-ce docker-ce-cli containerd.io
 
-echo "systemctl start docker"
+systemctl start docker
 
-echo "systemctl enable docker"
+systemctl enable docker
 
-echo "systemctl status docker"
+systemctl status docker
 
 echo "Done"
